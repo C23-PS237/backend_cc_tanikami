28476d99ac60fb1e1ebdff19b98dc96b3906155e
hiskandar <hiskandaryapis@gmail.com>
2023-05-21 13:27:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    console.log("Response success")
+    res.send("Response Success!")
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log("Server is up and listening on " + PORT)
+})
